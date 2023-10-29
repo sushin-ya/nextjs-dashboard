cdff55c9076513be16d830409dc5cb7b3bda5c4d
tsukaryu <drgon8otsuka@gmail.com>
2023-10-29 11:58:32 +0900

    finish ch 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,16 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const invoice = await fetchInvoiceById(id);
   const customers = await fetchCustomers();
 
-  if (invoice == undefined) return null;
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
